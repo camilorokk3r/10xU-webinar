4b7fc96fcf11e0be69375814f32166d039bccfd7
Camilo Martinez <camilo@rokk3rlabs.com>
2021-01-24 17:44:26 -0500

    Add login and signup with firebase

--- src/index.js
+++ src/index.js
@@ -1,42 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Welcome from './pages/welcome';
-import Login from './pages/login';
-import Signup from './pages/signup';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  useHistory
 } from "react-router-dom";
+
+import firebase from "firebase/app";
+import "firebase/auth";
+import { 
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer 
+} from '@react-firebase/auth';
+
+
+import { config } from "./config/config";
+
+
+import Welcome from './pages/welcome';
+import Login from './pages/login';
+import Signup from './pages/signup';
 import ToDo from './pages/todo';
 
 
 function App() {
+  
+  const history = useHistory();
+
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Welcome/>
-          </Route>
-          <Route path="/login">
-            <Login/>
-          </Route>
-          <Route path="/signup">
-            <Signup/>
-          </Route>
+    <FirebaseAuthProvider {...config} firebase={firebase}>
+      <React.Fragment>
+        <CssBaseline />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Welcome/>
+            </Route>
+            <Route path="/login">
+              <Login/>
+            </Route>
+            <Route path="/signup">
+              <Signup/>
+            </Route>
+          
+            <Route path="/todo">
+              <ToDo/>
+            </Route>
+
+            
+          </Switch>
+        </Router>
         
-          <Route path="/todo">
-            <ToDo/>
-          </Route>
-        </Switch>
-      </Router>
+      </React.Fragment>
       
-    </React.Fragment>
-    
+    </FirebaseAuthProvider>
   );
 }
 
